f5a11459cdd91f76e412de70326efdae5055c15e
fabionicolau <fabionicolaujr@gmail.com>
2022-10-28 15:16:34 -0300

    feat: implementa o teste para o relatório invidivual do Produto"

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -4,17 +4,17 @@ from inventory_report.inventory.product import Product
 def test_cria_produto():
     product = Product(
         1,
-        "nome_do_produto",
-        "nome_da_empresa",
-        "2022-10-28",
-        "2024-10-28",
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
         "123456",
-        "instrucoes_de_armazenamento",
+        "ao abrigo de luz",
     )
     assert product.id == 1
-    assert product.nome_do_produto == "nome_do_produto"
-    assert product.nome_da_empresa == "nome_da_empresa"
-    assert product.data_de_fabricacao == "2022-10-28"
-    assert product.data_de_validade == "2024-10-28"
+    assert product.nome_do_produto == "farinha"
+    assert product.nome_da_empresa == "Farinini"
+    assert product.data_de_fabricacao == "01-05-2021"
+    assert product.data_de_validade == "02-06-2023"
     assert product.numero_de_serie == "123456"
-    assert product.instrucoes_de_armazenamento == "instrucoes_de_armazenamento"
+    assert product.instrucoes_de_armazenamento == "ao abrigo de luz"
